6b407384ac6cc9c4fa07a320d78cdfde0435295c
egustavo <msmain34@yahoo.com>
2017-11-18 01:03:30 -0600

    added initial user interface components

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,41 @@
 package main;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class Main implements CommandLineRunner {
+public class Main extends Application {
+
+    private Parent root;
+    private ConfigurableApplicationContext springContext;
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class);
+        launch();
     }
 
     @Override
-    public void run(String... strings) throws Exception {
+    public void init() throws Exception {
 
+        // Get the spring context
+        springContext = SpringApplication.run(Main.class);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+
+        // Tell JavaFX to use the spring factory to create the controller(s)
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        root = fxmlLoader.load();
+        super.init();
+    }
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Cougar CRMS");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
