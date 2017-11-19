4582c6455b892cf061546952f35b280db55c6969
egustavo <msmain34@yahoo.com>
2017-11-19 00:24:56 -0600

    file renaming and scene change

---
+++ src/main/java/main/MainPageController.java
@@ -0,0 +1,47 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class MainPageController implements Initializable {
+
+    @FXML
+    private MenuBar menubarMenu;
+
+    @FXML
+    private Button buttonManagement;
+
+    @FXML
+    private Button buttonSales;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void changeScreenEmployeeManagement(ActionEvent actionEvent) throws IOException {
+
+        Node managementOptionsNode = (Node) actionEvent.getSource();
+        Stage managementOptionsStage = (Stage) managementOptionsNode.getScene().getWindow();
+        Parent managementOptionsParent = FXMLLoader.load(getClass().getResource("managementOptions.fxml"));
+        Scene managementOptionsScene = new Scene(managementOptionsParent);
+
+        managementOptionsStage.setScene(managementOptionsScene);
+        managementOptionsStage.show();
+
+    }
+}
---
+++ src/main/java/main/ManagementOptionsController.java
@@ -0,0 +1,26 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManagementOptionsController implements Initializable {
+
+    @FXML
+    private MenuBar menubarMenu;
+
+    @FXML
+    private Button buttonEmployeeManagement;
+
+    @FXML
+    private Button buttonViewReports;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
