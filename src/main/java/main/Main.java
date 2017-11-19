cbf611d8a47223f96785a194e20e94054225c916
egustavo <msmain34@yahoo.com>
2017-11-19 00:22:24 -0600

    file renaming and cleanup

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,9 +2,9 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -24,7 +24,7 @@ public class Main extends Application {
 
         // Get the spring context
         springContext = SpringApplication.run(Main.class);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainPage.fxml"));
 
         // Tell JavaFX to use the spring factory to create the controller(s)
         fxmlLoader.setControllerFactory(springContext::getBean);
@@ -33,7 +33,8 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(javafx.stage.Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+
         primaryStage.setTitle("Cougar CRMS");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
